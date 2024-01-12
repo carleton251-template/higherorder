ca2b7d599cdfa0946660a114721cc05b3b32250b
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-12 11:26:44 -0600

    Fixed testing scripts

--- tests-e.scm
+++ tests-e.scm
@@ -21,4 +21,9 @@
 
 (test-assert "exists3-ne" (not (exists (lambda (x) (< x 3)) '())))
 
-(test-equal "find-docs3" '()               (find-docs 'schiller doclist))
+(let ((doclist '((cs (Welcome to the Carleton CS department))
+                  (art (The department consists of two separate majors))
+                  (geology (The geology department retains a spirit of exploration))
+                  (president (Alison R Byerly is Carleton College’s 12th president)))))
+
+  (test-equal "find-docs3" '()               (find-docs 'schiller doclist)))
--- tests-m.scm
+++ tests-m.scm
@@ -13,10 +13,10 @@
 (test-equal "exists1" #t (exists (lambda (x) (< x 3)) '(9 2 1 8 7)))
 (test-equal "exists2" #t (not (exists (lambda (x) (< x -5)) '(-1 0 1))))
 
-(define doclist '((cs (Welcome to the Carleton CS department))
+(let ((doclist '((cs (Welcome to the Carleton CS department))
                   (art (The department consists of two separate majors))
                   (geology (The geology department retains a spirit of exploration))
-                  (president (Alison R Byerly is Carleton College’s 12th president))))
+                  (president (Alison R Byerly is Carleton College’s 12th president)))))
 
-(test-equal "find-docs1" '(cs art geology) (find-docs 'department doclist))
-(test-equal "find-docs2" '(cs president)   (find-docs 'Carleton doclist))
+  (test-equal "find-docs1" '(cs art geology) (find-docs 'department doclist))
+  (test-equal "find-docs2" '(cs president)   (find-docs 'Carleton doclist)))
